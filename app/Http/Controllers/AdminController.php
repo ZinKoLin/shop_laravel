6d9e4cf57e809557e0354df73989f41c5e45ff49
ZinKoLin <zinkolin113@gmail.com>
2023-09-21 19:19:51 +0630

    admin profile photo show2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
+
+
 
     public function adminLogin(){
         return view('admin.admin_login');
@@ -25,6 +28,12 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/admin/login');
+    }
+
+    public function adminProfile(){
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth','role:admin'])->group(function (){
     Route::get('/admin/dashboard',[AdminController::class,'adminDashboard'])->name('#admin.dashboard');
     Route::get('/admin/logout',[AdminController::class,'adminDestroy'])->name('#admin.logout');
+    Route::get('/admin/profile',[AdminController::class,'adminProfile'])->name('#admin.profile');
 
 
 });
