96fd9870feb40f9ccdb7c1956942afacd473a5b0
ZinKoLin <zinkolin113@gmail.com>
2023-10-20 15:19:29 +0630

    addproduct save mvc

--- routes/web.php
+++ routes/web.php
@@ -154,8 +154,9 @@ Route::middleware(['auth'])->group(function() {
 
     // Product All Route
     Route::controller(ProductController::class)->group(function(){
-        Route::get('/all/product' , 'AllProduct')->name('all.product');
-        Route::get('/add/product' , 'AddProduct')->name('add.product');
+        Route::get('/all/product' , 'allProduct')->name('all.product');
+        Route::get('/add/product' , 'addProduct')->name('add.product');
+        Route::post('/store/product' , 'storeProduct')->name('store.product');
 
 
 
